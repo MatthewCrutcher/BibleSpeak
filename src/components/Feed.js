812642f81b343aa57b91b2e5a090d47a4055263e
MatthewCrutcher <matthew.crutcher@hotmail.com>
2022-05-15 16:52:03 +0800

    feed page is flexible

---
+++ src/components/Feed.js
@@ -0,0 +1,30 @@
+import React from "react";
+//Styling
+import "./Feed.css";
+import Line from "../images/Line.png";
+
+function Feed() {
+  return (
+    <div>
+      <div className="feed-page">
+        <button className="question-button">
+          <h4>Ask a Question</h4>
+        </button>
+        <img className="line-seperator" src={Line} alt="Line Seperator" />
+        <div className="post-container">
+          <h3>Question</h3>
+          <div className="post-text-container">
+            <p>Example Text</p>
+          </div>
+
+          <p className="post-date">25/07/2001</p>
+          <button className="answer-button">
+            <h4>Answer</h4>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
