be135edcba302f49ef6c795b3106a9fe74c60431
MatthewCrutcher <matthew.crutcher@hotmail.com>
2022-05-20 16:51:55 +0800

    boiler plate for feed to answer is set up

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,20 +1,110 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 //Styling
 import "./Feed.css";
 import Line from "../images/Line.png";
 import Navbar from "./Navbar";
 import Error from "./Error";
+import Remove from "../images/remove.png";
+//Server / API
+import post from "../server/server";
+import answer from "../server/server";
 
 function Feed() {
   const [error, setError] = useState("");
-  const [question, setQuestion] = useState("");
+  const [question, setQuestion] = useState({ text: "", userId: 0 });
+  const [postInDB, setPostInDB] = useState([]);
+  const [answers, setAnswers] = useState([]);
+  //Using a dummy logged in ID for now
+  const [loggedIn, setLoggedIn] = useState(1);
+
+  useEffect(() => {
+    const postApiCall = async () => {
+      try {
+        const res = await post.get("/post");
+        setPostInDB(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    const answersApiCall = async () => {
+      try {
+        const res = await answer.get("/answer");
+        setAnswers(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    postApiCall();
+    answersApiCall();
+  }, []);
+
+  const mapPost = postInDB.map((val) => {
+    let combineAnswers = [];
+    answers.map((value) => {
+      if (val.id === value.postId) {
+        value.scripture.map((item) => {
+          combineAnswers.push(item);
+        });
+      }
+    });
+
+    const displayAnswers = combineAnswers.map((val) => {
+      return <div>{val}</div>;
+    });
+
+    return (
+      <>
+        {" "}
+        <img className="line-seperator" src={Line} alt="Line Seperator" />
+        <div className="post-container">
+          <h3>Question:</h3>
+
+          <div className="post-text-container">
+            <p>{val.text}</p>
+          </div>
+          <div className="post-date-remove-container">
+            <p className="post-date">25/07/2001</p>
+            <img
+              src={Remove}
+              alt="Remove Post"
+              className={
+                loggedIn === val.userId ? "remove-post" : "remove-post-none"
+              }
+              onClick={() => handleDelete(val.id)}
+            />
+          </div>
+          <button className="answer-button">
+            <h4>Answer</h4>
+          </button>
+        </div>
+        <div className="post-container">
+          <h3>Answers:</h3>
+          <div className="post-text-container">
+            <p>ANSWERS HERE</p>
+            {displayAnswers}
+          </div>
+        </div>
+      </>
+    );
+  });
+
+  const handleDelete = (id) => {
+    post.delete(`/post/${id}`).then((res) => {
+      console.log(id);
+    });
+
+    return null;
+  };
 
   const handleQuestion = (event) => {
     event.preventDefault();
     if (question === "") {
       setError("Question Cannot Be Empty!");
     } else {
-      window.location.reload();
+      post.post("/post", question).then((res) => {
+        console.log(res);
+        window.location.reload();
+      });
     }
   };
 
@@ -28,7 +118,11 @@ function Feed() {
             className="text-area"
             placeholder="Enter Your Question Here."
             onChange={(event) => {
-              setQuestion(event.target.value);
+              setQuestion({
+                ...question,
+                text: event.target.value,
+                userId: loggedIn,
+              });
             }}
           />
           <Error error={error} />
@@ -36,18 +130,7 @@ function Feed() {
             <h4>Post</h4>
           </button>
         </form>
-
-        <img className="line-seperator" src={Line} alt="Line Seperator" />
-        <div className="post-container">
-          <h3>Question</h3>
-          <div className="post-text-container">
-            <p>Example Text</p>
-          </div>
-          <p className="post-date">25/07/2001</p>
-          <button className="answer-button">
-            <h4>Answer</h4>
-          </button>
-        </div>
+        {mapPost}
       </div>
     </div>
   );
