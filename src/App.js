9bda5cd59f9e90cc9ee62653db78b28f3d856165
MatthewCrutcher <matthew.crutcher@hotmail.com>
2022-05-27 09:30:54 +0800

    may need to look deeper on keeping data on refresh

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, createContext } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 //Components
 import LandingPage from "./components/LandingPage";
@@ -10,17 +10,23 @@ import Answer from "./components/Answer";
 //Styling
 import "./App.css";
 
+export const LoggedInContext = createContext(null);
+
 function App() {
+  const [loggedIn, setLoggedIn] = useState(0);
+
   return (
     <Router>
-      <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/signup" element={<SignUp />} />
-        <Route exact path="/feed" element={<Feed />} />
-        <Route exact path="/answer" element={<Answer />} />
-      </Routes>
-      <Footer />
+      <LoggedInContext.Provider value={{ loggedIn, setLoggedIn }}>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<SignUp />} />
+          <Route exact path="/feed" element={<Feed />} />
+          <Route exact path="/answer" element={<Answer />} />
+        </Routes>
+        <Footer />
+      </LoggedInContext.Provider>
     </Router>
   );
 }
