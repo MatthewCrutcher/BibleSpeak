e4aae2bc2eb1fc2f9c4e9f6654e422da788910f8
MatthewCrutcher <matthew.crutcher@hotmail.com>
2022-05-30 14:19:44 +0800

    mapped through like fed page to display posts that the user has created, might remove function to see answers only

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,95 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 //Styling
 import "./Profile.css";
 import Navbar from "./Navbar";
+import Line from "../images/Line.png";
+import Remove from "../images/remove.png";
+import "./Feed.css";
+//Server
+import post from "../server/server";
+import answer from "../server/server";
 
 function Profile() {
   const [selection, setSelection] = useState(false);
+  const [postInDB, setPostInDB] = useState([]);
+  const [answerInDB, setAnswerInDB] = useState([]);
+  const [loggedIn, setLoggedIn] = useState(1);
+
+  useEffect(() => {
+    const postApiCall = async () => {
+      try {
+        const res = await post.get("/post");
+        setPostInDB(res.data);
+        console.log(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    const answerApiCall = async () => {
+      try {
+        const res = await answer.get("/answer");
+        setAnswerInDB(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    postApiCall();
+    answerApiCall();
+  }, []);
+
+  // const mapAnswers = answerInDB.map((val) => {
+  //   if
+  // })
+
+  const mapPost = postInDB.map((val) => {
+    if (val.userId === loggedIn) {
+      const mappingAnswers = answerInDB.map((value) => {
+        if (val.id === value.postId) {
+          const mappingScripture = value.scripture.map((scrip) => {
+            return <div>{scrip}</div>;
+          });
+          return (
+            <div className="mapped-answers-container">{mappingScripture}</div>
+          );
+        }
+      });
+      return (
+        <div className="mapping-outer-container">
+          <img className="line-seperator" src={Line} alt="Line Seperator" />
+          <div className="post-container">
+            <h3>Question:</h3>
+
+            <div className="post-text-container">
+              <p>{val.text}</p>
+            </div>
+            <div className="post-date-remove-container">
+              <p className="post-date">25/07/2001</p>
+              <img
+                src={Remove}
+                alt="Remove Post"
+                className="remove-post"
+
+                // onClick={() => handleDelete(val.id, "post")}
+              />
+            </div>
+            <button
+              className="answer-button"
+              // onClick={() => navigate("/answer")}
+            >
+              <h4>Answer</h4>
+            </button>
+          </div>
+          <div className="post-container">
+            <h3>Answers:</h3>
+            <div className="mapped-answers-outer-container">
+              {mappingAnswers}
+            </div>
+          </div>
+        </div>
+      );
+    }
+  });
+
   return (
     <div>
       <Navbar />
@@ -18,7 +103,7 @@ function Profile() {
             }
             onClick={() => setSelection(false)}
           >
-            Questions
+            <h4>Questions</h4>
           </button>
           <button
             className={
@@ -28,7 +113,7 @@ function Profile() {
             }
             onClick={() => setSelection(true)}
           >
-            Answers
+            <h4>Answers</h4>
           </button>
         </div>
         <h2
@@ -47,6 +132,8 @@ function Profile() {
         >
           Answers You Have Given
         </h2>
+
+        {mapPost}
       </div>
     </div>
   );
